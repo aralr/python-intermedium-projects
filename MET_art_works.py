6ed07605a32d327e7368be8f445d7b2be708ec18
Arantza <ara.lr0104@gmail.com>
2026-01-16 17:09:51 -0600

    Create MET_art_works.py

---
+++ MET_art_works.py
@@ -0,0 +1,42 @@
+a_sunday_on_la_grande_jatte = {
+  'artist': 'Georges Seurat',
+  'period': 'Paris 1859–1891',
+  'date': '1884'
+}
+
+striding_thoth = {
+  'artist': 'unknown',
+  'period': 'Ptolemaic Period',
+  'date': '332–30 BCE'
+}
+
+jonah_and_the_whale = {
+  'artist': 'unknown',
+  'period': 'persian era',
+  'date': 'ca. 1400'
+}
+
+garden_gathering = {
+  'artist': 'unknown',
+  'period': '1587–1629',
+  'date': '1640–50'
+}
+
+nude_in_front_of_a_mantel = {
+  'artist': 'Balthasar Klossowski',
+  'period': 'modernism',
+  'date': '1587–1629'
+}
+
+marbled_paper = {
+  'artist': 'Karli Frigge',
+  'period': 'modernism',
+  'date': '1985'
+}
+
+print(a_sunday_on_la_grande_jatte)
+print(striding_thoth)
+print(jonah_and_the_whale)
+print(garden_gathering)
+print(nude_in_front_of_a_mantel)
+print(marbled_paper)
